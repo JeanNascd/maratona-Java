b7a5872bd89db5b13a374d64886cdaac6d86227e
Jean Nascimento <jeandesouza15407@gmail.com>
2025-02-22 13:21:17 -0300

    Aula 4 Java

---
+++ Aula04Operadores.java
@@ -0,0 +1,8 @@
+package academy.devdojo.maratona.java;
+public class Aula04Operadores {
+    public static void main(String[] args) {
+        int numero01 = 10;
+        int numero02 = 02;
+        System.out.println(numero01+numero02*numero01);
+    }
+}
