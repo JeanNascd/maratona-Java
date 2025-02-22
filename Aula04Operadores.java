2dbece4a0923b7b575ff43e237e7cbd7b2ba331b
Jean Nascimento <jeandesouza15407@gmail.com>
2025-02-22 13:49:16 -0300

    aula4

--- Aula04Operadores.java
+++ Aula04Operadores.java
@@ -3,6 +3,7 @@ public class Aula04Operadores {
     public static void main(String[] args) {
         int numero01 = 10;
         int numero02 = 02;
-        System.out.println(numero01+numero02*numero01);
+        double resultado = numero01 / (double) numero02;
+        System.out.println(resultado);
     }
 }
