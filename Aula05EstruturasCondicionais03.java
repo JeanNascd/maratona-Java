7b2a0612651510d42b5607bd9d1590e3085a94d7
Jean Nascimento <jeandesouza15407@gmail.com>
2025-02-24 22:08:03 -0300

    OperadorTenario

--- Aula05EstruturasCondicionais03.java
+++ Aula05EstruturasCondicionais03.java
@@ -2,6 +2,14 @@ package academy.devdojo.maratona.java;
 
 public class Aula05EstruturasCondicionais03 {
     public static void main(String[] args) {
-        
+
+        // doar se salario > 5000
+        double salario = 6000;
+        String mensagemDoar = "Eu vou doar 500 pro DevDojo";
+        String mensagemNaoDoar = "Ainda não tenho condições, mas vou ter";
+        String resultado = salario > 5000 ? mensagemDoar : mensagemNaoDoar;
+
+        System.out.println(resultado);
     }
 }
+
