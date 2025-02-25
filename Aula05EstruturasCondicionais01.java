9bec9920d8d5d1a0c2d7ea31c46b750ccc69c0e1
Jean Nascimento <jeandesouza15407@gmail.com>
2025-02-24 21:42:28 -0300

    Aula05.3

--- Aula05EstruturasCondicionais01.java
+++ Aula05EstruturasCondicionais01.java
@@ -1,17 +1,20 @@
 package academy.devdojo.maratona.java;
 
-public class Aula05EstruturasCondicionais {
+public class Aula05EstruturasCondicionais01 {
     public static void main(String[] args) {
         int idade = 15;
         boolean isAutorizadoComprarBebida = idade >= 18;
         // !
-        if (isAutorizadoComprarBebida) {
+        if (isAutorizadoComprarBebida == false) {
             System.out.println("Pode beber bebida alcólica");
         }
         if(!isAutorizadoComprarBebida == false) {
             System.out.println("Não autorizado a comprar bebida alcólica");
         }
-
+        boolean c = false;
+        if(c == true){
+            System.out.println("Dentro de algo que nunca deve ser feito");
+        }
         System.out.println("Fora do if");
     }
 }
---
+++ Aula05EstruturasCondicionais03.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratona.java;
+
+public class Aula05EstruturasCondicionais03 {
+    public static void main(String[] args) {
+        
+    }
+}
