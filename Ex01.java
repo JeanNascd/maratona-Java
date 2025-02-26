927f49a28d20b89afddebc291384218c60cc810b
Jean Nascimento <jeandesouza15407@gmail.com>
2025-02-25 22:26:14 -0300

    Switch Case

--- Ex01.java
+++ Ex01.java
@@ -2,16 +2,19 @@ package academy.devdojo.maratona.java;
 
 public class Ex01 {
     public static void main(String[] args) {
-        double salarioAnual = 50.000F;
+        double salarioAnual = 70000;
         double taxa;
-        if(salarioAnual <= 34.712){
-            taxa = 9.70;
-        }else if (salarioAnual <= 68.508){
-            taxa = 37.35;
+        double valorImposto = 0;
+        if(salarioAnual <= 34712){
+            taxa = 9.70 / 100;
+        }else if (salarioAnual <= 68508){
+            taxa = 37.35 / 100;
         }
         else{
-            taxa = 49.50;
+            taxa = 49.50 / 100;
         }
+        valorImposto = salarioAnual * taxa;
+        System.out.println(valorImposto);
         System.out.println(taxa);
     }
 }
